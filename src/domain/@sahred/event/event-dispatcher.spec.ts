fe006c1f3d936f76c5ff4186f6c7664aa212ea5c
venilson2 <venilson2009@hotmail.com>
2024-11-02 14:10:11 -0300

    feat(event): add tests

--- src/domain/@sahred/event/event-dispatcher.spec.ts
+++ src/domain/@sahred/event/event-dispatcher.spec.ts
@@ -1,12 +1,42 @@
+import { CustomerCreatedEvent } from "../../customer/entities/customer-created.event";
 import SendConsoleLog1Handler from "../../customer/events/handle/send-console-log-1.handler";
 import EventDispatcher from "./event-dispatcher";
 
 describe('Domain events tests', () => {
   
-
   it('should register an event handler', () => {
     const eventDispatcher = new EventDispatcher();
     const eventHandler = new SendConsoleLog1Handler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    eventDispatcher.unregisterAll();
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeUndefined();
   })
 
+  it('should dispatch an event handlers', () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendConsoleLog1Handler();
+    
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    const customerCreatedEvent = new CustomerCreatedEvent();
+
+    eventDispatcher.dispatch(customerCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  })
 })
